a21a300cc0be3c3b5fc82558d572c357bf78a394
PranshuBasak <pranshubasak@gmail.com>
2024-02-19 01:13:24 +0530

    Models changed

--- lib/database/models/transaction.model.ts
+++ lib/database/models/transaction.model.ts
@@ -1,14 +1,5 @@
 import { Schema, model, models } from "mongoose";
 
-// itransaction interface for TransactionSchema
-export interface ITransaction {
-    createdAt?: Date; // 1. Date and time when the transaction was created, defaults to current date if not provided (optional)
-    stripeId: string; // 2. Stripe ID of the transaction
-    amount: number; // 3. Amount of the transaction
-    plan?: string; // 4. Plan associated with the transaction (optional)
-    credits?: number; // 5. Credits associated with the transaction (optional)
-    buyer: Schema.Types.ObjectId; // 6. Buyer of the transaction, referencing the 'User' schema
-}
 
 
 const TransactionSchema = new Schema({
--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,17 +1,6 @@
-import { Document, Schema, model, models } from "mongoose";
+import { Schema, model, models } from "mongoose";
 
 
-// iuser interface for UserSchema
-export interface IUser extends Document {
-    clerkId: string; // 1. Clerk ID of the user
-    email: string; // 2. Email of the user
-    username: string; // 3. Username of the user
-    photo: string; // 4. URL of the user's photo
-    firstName?: string; // 5. First name of the user (optional)
-    lastName?: string; // 6. Last name of the user (optional)
-    planId?: number; // 7. Plan ID of the user, defaulting to 1 if not provided (optional)
-    creditBalance?: number; // 8. Credit balance of the user, defaulting to 10 if not provided (optional)
-}
 
 
 
